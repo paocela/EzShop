f0c2d9b17002f71ca61603b0424f06446ce4fed4
luca.pezzolla <me@lucapezzolla.com>
2021-05-16 12:18:42 +0200

    fix(EZShop): Changed method signature to allow test visibility for methods ::byteToHex, ::hashPassword

--- src/main/java/it/polito/ezshop/data/EZShop.java
+++ src/main/java/it/polito/ezshop/data/EZShop.java
@@ -2269,7 +2269,7 @@ public class EZShop implements EZShopInterface {
         }
     }
 
-    private String hashPassword(String password) {
+    public static String hashPassword(String password) {
         String hashedPassword = null;
         try {
             MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
@@ -2280,7 +2280,7 @@ public class EZShop implements EZShopInterface {
         return hashedPassword;
     }
 
-    private String byteToHex(final byte[] hash) {
+    public static String byteToHex(final byte[] hash) {
         Formatter formatter = new Formatter();
         for (byte b : hash) {
             formatter.format("%02x", b);
